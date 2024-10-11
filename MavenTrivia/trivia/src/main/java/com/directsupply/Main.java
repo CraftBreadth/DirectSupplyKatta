743d3076951cf03f63a46f96b3bfe5d063cebf19
CraftBreadth <sorenspj@rose-hulman.edu>
2024-10-11 18:25:15 -0500

    High Level parsing done

--- MavenTrivia/trivia/src/main/java/com/directsupply/Main.java
+++ MavenTrivia/trivia/src/main/java/com/directsupply/Main.java
@@ -3,26 +3,93 @@ package com.directsupply;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
 import java.net.http.HttpClient;
 import java.net.URI;
 import java.io.IOException;
 import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import java.nio.charset.StandardCharsets;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(CreateURL(1, QuestionType.COMBINED))).build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        System.out.println("response body: " + response.body());
         System.out.println("Hello world!");
-        String json = "...";
+        JSONtoQuestionList(CreateURL(1, QuestionType.MULTIPLECHOICE), 10);
 
-        Gson gson = new Gson();
     }
 
     public static String CreateURL(int numQuestions, QuestionType type) {
-        String outputString = "https://opentdb.com/api.php?amount=10&type=multiple";
+        String outputString = "https://opentdb.com/api.php?amount=10&encode=url3986";
         return outputString;
+    }
+
+    public static Question[] JSONtoQuestionList(String JSONURL, int numberOfQuestions) throws IOException {
+        String jsonAddress = IOUtils.toString(new URL(JSONURL), StandardCharsets.ISO_8859_1);
+        JsonReader reader = new JsonReader(new StringReader(jsonAddress));
+        Question[] questionList = new Question[numberOfQuestions];
+        reader.beginObject();
+        System.out.println("Ready to Read!");
+        while (!reader.peek().equals(JsonToken.BEGIN_ARRAY)) {
+            if (!reader.hasNext()) {
+                System.out.println("String Parsed");
+            }
+            reader.skipValue();
+        }
+        System.out.println("Done Reading!");
+        reader.beginArray();
+        int questionIndex = 0;
+        JsonToken token = reader.peek();
+        while (true) {
+            token = reader.peek();
+            if (token.equals(JsonToken.END_ARRAY)) {
+                reader.endArray();
+                break;
+            } else if (token.equals(JsonToken.BEGIN_OBJECT)) {
+                reader.beginObject();
+                // Check Question Type
+                while (!reader.peek().equals(JsonToken.END_OBJECT)) {
+                    // reader.skipValue();
+                    token = reader.peek();
+                    if (token.equals(JsonToken.NAME)) {
+                        String tokenName = reader.nextName();
+                        System.out.println(tokenName);
+                        if (tokenName.equals("type")) {
+                            break;
+                        } else {
+                            System.out.println("error");
+                        }
+                    } else {
+                        System.out.println("error");
+                    }
+                }
+                Question question = new Question();
+                token = reader.peek();
+                String typeName = reader.nextString();
+                if (typeName.equals("multiple")) {
+                    question = new MCQuestion(reader);
+                    System.out.println("Creating a MC question");
+                } else if (typeName.equals("boolean")) {
+                    question = new TFQuestion(reader);
+                    System.out.println("Creating a MC question");
+                } else {
+                    System.out.println("An error has occured!");
+                }
+                questionList[questionIndex] = question;
+            } else if (token.equals(JsonToken.END_OBJECT)) {
+                reader.endObject();
+            } else {
+                // handleNonArrayToken(reader, token);
+            }
+            questionIndex++;
+        }
+        return questionList;
     }
 }
--- MavenTrivia/trivia/src/main/java/com/directsupply/Question.java
+++ MavenTrivia/trivia/src/main/java/com/directsupply/Question.java
@@ -1,16 +1,153 @@
 package com.directsupply;
+
+import java.io.IOException;
+
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
 public class Question {
-    int rightAnswerNumber;
-    String questionString;
-    int difficulty;
-    String category;
-    String correctAnswer;
+    private int rightAnswerNumber;
+    private String difficulty;
+    private String questionString;
+    private String category;
+    private String correctAnswer;
+
+    /**
+     * Handle non array non object tokens
+     * 
+     * @param reader
+     * @param token
+     * @throws IOException
+     */
+    public static void handleNonArrayToken(JsonReader reader, JsonToken token) throws IOException {
+        if (token.equals(JsonToken.NAME))
+            System.out.println(reader.nextName());
+        else if (token.equals(JsonToken.STRING))
+            System.out.println(reader.nextString());
+        else if (token.equals(JsonToken.NUMBER))
+            System.out.println(reader.nextDouble());
+        else
+            reader.skipValue();
+    }
+
+    /**
+     * Handle a json array. The first token would be JsonToken.BEGIN_ARRAY.
+     * Arrays may contain objects or primitives.
+     * 
+     * @param reader
+     * @throws IOException
+     */
+    public static void handleArray(JsonReader reader) throws IOException {
+        reader.beginArray();
+        while (true) {
+            JsonToken token = reader.peek();
+            if (token.equals(JsonToken.END_ARRAY)) {
+                reader.endArray();
+                break;
+            } else if (token.equals(JsonToken.BEGIN_OBJECT)) {
+                handleObject(reader);
+            } else if (token.equals(JsonToken.END_OBJECT)) {
+                reader.endObject();
+            } else
+                handleNonArrayToken(reader, token);
+        }
+    }
+
+    /**
+     * Handle an Object. Consume the first token which is BEGIN_OBJECT. Within
+     * the Object there could be array or non array tokens. We write handler
+     * methods for both. Noe the peek() method. It is used to find out the type
+     * of the next token without actually consuming it.
+     * 
+     * @param reader
+     * @throws IOException
+     */
+    private static void handleObject(JsonReader reader) throws IOException {
+        reader.beginObject();
+        while (reader.hasNext()) {
+            JsonToken token = reader.peek();
+            if (token.equals(JsonToken.BEGIN_ARRAY))
+                handleArray(reader);
+            else if (token.equals(JsonToken.END_OBJECT)) {
+                reader.endObject();
+                return;
+            } else
+                handleNonArrayToken(reader, token);
+        }
+
+    }
+
+    public Question(JsonReader reader) throws IOException {
+        // TODO Auto-generated constructor stub
+
+
+    }
+
+    public Question() {
+        //TODO Auto-generated constructor stub
+    }
+
+    public int getRightAnswerNumber() {
+        return this.rightAnswerNumber;
+
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setRightAnswerNumber(int rightAnswerNumber) {
+        this.rightAnswerNumber = rightAnswerNumber;
+    }
+
+    public void setQuestionString(String questionString) {
+        this.questionString = questionString;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setValuesBasedOnJSON(JsonReader reader) {
+
+    }
+
+    public String getDifficulty() {
+        return this.difficulty;
+    }
+
+    public String getQuestionString() {
+        return this.questionString;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public String getCorrectAnswer() {
+        return this.correctAnswer;
+    }
+
 }
 
 class TFQuestion extends Question {
-    String incorrectAnswer;
+    public TFQuestion(JsonReader reader) throws IOException {
+        super(reader);
+        //TODO Auto-generated constructor stub
+    }
+
+    private String incorrectAnswer;
+
 }
 
 class MCQuestion extends Question {
-    String[] incorrectAnswers;
+    public MCQuestion(JsonReader reader) {
+        //TODO Auto-generated constructor stub
+    }
+
+    private String[] incorrectAnswers;
 }
