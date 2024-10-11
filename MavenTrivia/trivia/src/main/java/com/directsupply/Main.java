055b5eb71d9f7cec354bb74effc9bb1d77c30699
CraftBreadth <sorenspj@rose-hulman.edu>
2024-10-10 22:05:32 -0500

    Created Maven Project

---
+++ MavenTrivia/trivia/src/main/java/com/directsupply/Main.java
@@ -0,0 +1,13 @@
+package com.directsupply;
+
+import com.google.gson.Gson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String json = "...";
+
+        Gson gson = new Gson();
+    }
+}
