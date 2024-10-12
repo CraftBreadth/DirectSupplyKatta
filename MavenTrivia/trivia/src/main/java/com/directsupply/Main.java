de92c731c68a398e42ab508e133edc1e9d68430c
CraftBreadth <sorenspj@rose-hulman.edu>
2024-10-11 19:53:51 -0500

    Updated to extract each question using the gson.fromJson method

--- MavenTrivia/trivia/src/main/java/com/directsupply/Main.java
+++ MavenTrivia/trivia/src/main/java/com/directsupply/Main.java
@@ -32,64 +32,44 @@ public class Main {
     }
 
     public static Question[] JSONtoQuestionList(String JSONURL, int numberOfQuestions) throws IOException {
-        String jsonAddress = IOUtils.toString(new URL(JSONURL), StandardCharsets.ISO_8859_1);
-        JsonReader reader = new JsonReader(new StringReader(jsonAddress));
+        String jsonStream = IOUtils.toString(new URL(JSONURL), StandardCharsets.ISO_8859_1);
         Question[] questionList = new Question[numberOfQuestions];
-        reader.beginObject();
-        System.out.println("Ready to Read!");
-        while (!reader.peek().equals(JsonToken.BEGIN_ARRAY)) {
-            if (!reader.hasNext()) {
-                System.out.println("String Parsed");
-            }
-            reader.skipValue();
-        }
+
         System.out.println("Done Reading!");
-        reader.beginArray();
-        int questionIndex = 0;
-        JsonToken token = reader.peek();
-        while (true) {
-            token = reader.peek();
-            if (token.equals(JsonToken.END_ARRAY)) {
-                reader.endArray();
-                break;
-            } else if (token.equals(JsonToken.BEGIN_OBJECT)) {
-                reader.beginObject();
-                // Check Question Type
-                while (!reader.peek().equals(JsonToken.END_OBJECT)) {
-                    // reader.skipValue();
-                    token = reader.peek();
-                    if (token.equals(JsonToken.NAME)) {
-                        String tokenName = reader.nextName();
-                        System.out.println(tokenName);
-                        if (tokenName.equals("type")) {
-                            break;
-                        } else {
-                            System.out.println("error");
-                        }
-                    } else {
-                        System.out.println("error");
-                    }
+        int endQuestionIndex = 0;
+        //Avoid first curly brace
+        int startQuestionIndex = 0;
+        // Loop through each question
+        for (int questionIndex = 0; questionIndex < numberOfQuestions; questionIndex++) {
+
+            // Do Do string parser
+            // Find First curly index and second cury brace index
+            int currentIndex = endQuestionIndex+1;
+            while (true) {
+                if (jsonStream.charAt(currentIndex)=='{'){
+                    startQuestionIndex = currentIndex;
+                    currentIndex++;
+                } else if (jsonStream.charAt(currentIndex) == '}'){
+                    endQuestionIndex = currentIndex;
+                    break;
+                } else{
+                    currentIndex++;
                 }
-                Question question = new Question();
-                token = reader.peek();
-                String typeName = reader.nextString();
-                if (typeName.equals("multiple")) {
-                    question = new MCQuestion(reader);
-                    System.out.println("Creating a MC question");
-                } else if (typeName.equals("boolean")) {
-                    question = new TFQuestion(reader);
-                    System.out.println("Creating a MC question");
-                } else {
-                    System.out.println("An error has occured!");
-                }
-                questionList[questionIndex] = question;
-            } else if (token.equals(JsonToken.END_OBJECT)) {
-                reader.endObject();
-            } else {
-                // handleNonArrayToken(reader, token);
             }
-            questionIndex++;
+            String individualQuestionString = jsonStream.substring(startQuestionIndex, endQuestionIndex+1);
+
+            // Find Second Curly index
+
+            // Split string
+            // Move to next string.
+
+            Gson gson = new Gson();
+            //Question question = gson.fromJson(individualQuestionString, Question.class);
+            Question question = gson.fromJson(individualQuestionString, Question.class);
+            System.out.println("Creating a MC question");
+            questionList[questionIndex] = question;
         }
+
         return questionList;
     }
 }
